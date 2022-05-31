628ba5e7f1210790afbead7324db9c7c186ba1c4
DaaliVardhan <96312466+DaaliVardhan@users.noreply.github.com>
2022-05-31 22:16:50 +0530

    Radix Sort uploaded

---
+++ python/Radix Sort/radixsort.py
@@ -0,0 +1,79 @@
+
+# Function to take user input
+def vsduserinput():
+    n=int(input("Enter the size: "))
+    print("Enter the numbers to sort with spaces between: ")
+    s=input().split(" ")
+    a=[int(s[i]) for i in range(n)]
+
+    return a,n
+
+# Function to get maximum element in the array:
+def vsdmax(arr: list):
+    return max(arr)
+
+
+# Function to perform counting sort
+def vsdcountingsort(arr: list,decimalplace:int ):
+    
+    size=len(arr)
+
+    # Create count array of length 10 (0-9 are decimal literals)
+    count=[0 for i in range(10)]
+
+    # Create a output array to hold sorted elements
+    output=[None for i in range(size)]
+    
+    # Iterate through the  given array
+    for i in range(size):
+
+        # Find the element according to the decimal place likes ones,tens...
+        index=arr[i]//decimalplace
+
+        # Count each element and store in count array 
+        # mod 10 is used to isolate element from the number
+        count[index%10]+=1
+        
+    # Iterate through the count array 
+    # and add previous element value to current element
+    for i in range(1,len(count)):
+        count[i]+=count[i-1]
+
+    # Iterate from end of outpurt array
+    # Find the max element and place it in the output array
+    for i in range(size-1,-1,-1):
+        index=arr[i]//decimalplace
+        output[count[index%10]-1]=arr[i]
+        count[index%10]-=1
+    
+    # Replace the element of given array from output array
+    for i in range(size):
+        arr[i]=output[i]
+
+
+# Function to perform radix sort
+def vsdradixsort(arr:list):
+
+    # Find the max element in array
+    m=max(arr)
+
+    # Start from the ones place 
+    # Sort the elements using counting sort on place value
+    decimalplace=1
+    while m//decimalplace >0:
+        vsdcountingsort(arr,decimalplace)
+        decimalplace*=10
+
+# Function to print output
+def vsdprintarray(array):
+    for i in array:
+        print(i,end=" ")
+    print()
+
+if __name__=="__main__":
+    array,n=vsduserinput()
+    print("Given array: ")
+    vsdprintarray(array)
+    print("Sorted array: ")
+    vsdradixsort(array)
+    vsdprintarray(array)
