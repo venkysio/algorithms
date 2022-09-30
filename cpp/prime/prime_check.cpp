f1d81adc0018301420ad547f2453b38a814644c9
Sai Krishna <76392114+vemulasaikrishna03@users.noreply.github.com>
2022-09-30 22:49:07 +0530

    Add files via upload

---
+++ cpp/prime/prime_check.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+void check_prime(int n){
+    if(n==0 || n==1){
+        cout<<n<<" is a prime number";
+    }
+    else{
+        int ptr=1;
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0){
+                cout<<n<<" is not a prime number";
+                ptr=0;
+                break;
+                
+            }
+            
+        }
+        if(ptr){
+            cout<<n<<" is a prime number";        }
+        
+    }
+}
+int main() {
+    cout<<"enter a number to check prime ";
+    int n;
+    cin>>n;
+    cout<<endl;
+    check_prime(n);
+    return 0;
+}
---
+++ cpp/sort_min_heap/sort_min_heap.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int sort_min_heap(int arr[],int len){
+     priority_queue<int,vector<int>,greater<int>>minh;
+     for(int i=0;i<len;i++){
+         minh.push(arr[i]);
+     }
+     cout<<"This is the requird min heap sorted array ";
+     for(int i=0;i<len;i++){
+         cout<<minh.top()<<" ";
+         minh.pop();
+     }
+     
+}
+int main() {
+    int n;
+    cout<<"enter no of elements in array ";
+    cin >>n;
+    int arr[n];
+    cout<<"enter elements in the array";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort_min_heap(arr,n);
+
+    return 0;
+}
